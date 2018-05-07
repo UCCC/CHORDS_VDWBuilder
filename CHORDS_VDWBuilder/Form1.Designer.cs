7502e4ecef9e712c99b4040d6b8e05042bd9267d
Bryant Doyle <BRYANT.DOYLE@UCDENVER.EDU>
2018-05-07 08:30:39 -0600

    Initial Checkin

---
+++ CHORDS_VDWBuilder/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CHORDS_VDWBuilder
+{
+    partial class CHORD_VDWBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FHIR_URL_LBL = new System.Windows.Forms.Label();
+            this.FHIR_URL = new System.Windows.Forms.TextBox();
+            this.Log_LB = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.Import_FHIR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FHIR_URL_LBL
+            // 
+            this.FHIR_URL_LBL.AutoSize = true;
+            this.FHIR_URL_LBL.Location = new System.Drawing.Point(12, 20);
+            this.FHIR_URL_LBL.Name = "FHIR_URL_LBL";
+            this.FHIR_URL_LBL.Size = new System.Drawing.Size(60, 13);
+            this.FHIR_URL_LBL.TabIndex = 0;
+            this.FHIR_URL_LBL.Text = "FHIR URL:";
+            this.FHIR_URL_LBL.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FHIR_URL
+            // 
+            this.FHIR_URL.Location = new System.Drawing.Point(78, 17);
+            this.FHIR_URL.Name = "FHIR_URL";
+            this.FHIR_URL.Size = new System.Drawing.Size(1122, 20);
+            this.FHIR_URL.TabIndex = 1;
+            // 
+            // Log_LB
+            // 
+            this.Log_LB.FormattingEnabled = true;
+            this.Log_LB.Location = new System.Drawing.Point(12, 56);
+            this.Log_LB.Name = "Log_LB";
+            this.Log_LB.Size = new System.Drawing.Size(1188, 485);
+            this.Log_LB.TabIndex = 2;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 559);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1188, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // Import_FHIR
+            // 
+            this.Import_FHIR.Location = new System.Drawing.Point(15, 600);
+            this.Import_FHIR.Name = "Import_FHIR";
+            this.Import_FHIR.Size = new System.Drawing.Size(75, 23);
+            this.Import_FHIR.TabIndex = 4;
+            this.Import_FHIR.Text = "Import";
+            this.Import_FHIR.UseVisualStyleBackColor = true;
+            this.Import_FHIR.Click += new System.EventHandler(this.Import_FHIR_Click);
+            // 
+            // CHORD_VDWBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 646);
+            this.Controls.Add(this.Import_FHIR);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.Log_LB);
+            this.Controls.Add(this.FHIR_URL);
+            this.Controls.Add(this.FHIR_URL_LBL);
+            this.Name = "CHORD_VDWBuilder";
+            this.Text = "CHORDS VDW Builder";
+            this.Load += new System.EventHandler(this.CHORD_VDWBuilder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FHIR_URL_LBL;
+        private System.Windows.Forms.TextBox FHIR_URL;
+        private System.Windows.Forms.ListBox Log_LB;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button Import_FHIR;
+    }
+}
+
---
+++ CHORDS_VDWBuilder/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Model;
+using CHORDS_VDWBuilder.Models;
+
+namespace CHORDS_VDWBuilder
+{
+    public partial class CHORD_VDWBuilder : Form
+    {
+        
+
+        public CHORD_VDWBuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Import_FHIR_Click(object sender, EventArgs e)
+        {
+            // Connect to FHIR Server
+            var client = new FhirClient(FHIR_URL.Text);
+            client.PreferredFormat = ResourceFormat.Json;
+            client.Timeout = 120000;
+
+            Log_LB.Items.Add("Connected to: " + FHIR_URL.Text);
+
+            // Build list of Patients and Fill in Demographic Table
+            List<Patient> patients = loadPatients(client);
+
+            // Build Death Table
+            ;
+
+        }
+
+        // Load Valid Patients
+        private List<Patient> loadPatients(FhirClient iClient)
+        {
+            List<Patient> result = new List<Patient>();
+
+            if(iClient != null)
+            {
+                Bundle patients = iClient.Search<Patient>();
+
+                Log_LB.Items.Add("Number of Patients: " + patients.Entry.Count.ToString());
+
+                int successful = 0;
+
+                using (var context = new VDW_3_1_Entities())
+                {
+                    // Add Patients to VDW
+                    foreach (Bundle.EntryComponent item in patients.Entry)
+                    {
+                        Patient p = (Patient)item.Resource;
+
+                        if (p.Identifier.Count > 0)
+                        {
+                            // build demographics record
+                            DEMOGRAPHICS demo = new DEMOGRAPHICS();
+
+                            // PERSON_ID
+                            demo.PERSON_ID = p.Identifier.FirstOrDefault().Value;
+
+                            // MRN
+                            demo.MRN = p.Identifier.FirstOrDefault().Value;
+
+                            // BIRTH_DATE
+                            if (p.BirthDate != null)
+                            {
+                                demo.BIRTH_DATE = DateTime.ParseExact(p.BirthDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                            }
+
+                            // GENDER
+                            if (p.Gender == AdministrativeGender.Female)
+                            {
+                                demo.GENDER = "F";
+                            }
+                            if (p.Gender == AdministrativeGender.Male)
+                            {
+                                demo.GENDER = "M";
+                            }
+                            if (p.Gender == AdministrativeGender.Other)
+                            {
+                                demo.GENDER = "O";
+                            }
+                            if (p.Gender == AdministrativeGender.Unknown)
+                            {
+                                demo.GENDER = "U";
+                            }
+
+                            // PRIMARY_LANGUAGE and NEEDS_INTERPRETER
+                            if (p.Communication.Count > 0)
+                            {
+                                demo.PRIMARY_LANGUAGE = p.Communication.FirstOrDefault().Language.Text;
+                                if (p.Communication.FirstOrDefault().Preferred != null)
+                                {
+                                    demo.NEEDS_INTERPRETER = "S";
+                                }
+                            }
+                            else
+                            {
+                                demo.PRIMARY_LANGUAGE = "";
+                                demo.NEEDS_INTERPRETER = "";
+                            }
+
+                            if (p.Extension.Count() > 0)
+                            {
+                                // RACE1
+                                ;
+
+                                // RACE2
+                                ;
+
+                                // RACE3
+                                ;
+
+                                // RACE4
+                                ;
+
+                                // RACE5
+                                ;
+
+                                // HISPANIC
+                                ;
+
+                                // SEXUAL_ORIENTATION
+                                ;
+
+                                // GENDER_IDENTITY
+                                ;
+                            }
+                            else
+                            {
+                                // RACE1
+                                demo.RACE1 = "UN";
+
+                                // RACE2
+                                demo.RACE2 = "UN";
+
+                                // RACE3
+                                demo.RACE3 = "UN";
+
+                                // RACE4
+                                demo.RACE4 = "UN";
+
+                                // RACE5
+                                demo.RACE5 = "UN";
+
+                                // HISPANIC
+                                demo.HISPANIC = "U";
+
+                                // SEXUAL_ORIENTATION
+                                demo.SEXUAL_ORIENTATION = null;
+
+                                // GENDER_IDENTITY
+                                demo.GENDER_IDENTITY = null;
+                            }
+
+                            try
+                            {
+                                context.DEMOGRAPHICS.Add(demo);
+                                context.SaveChanges();
+
+                                result.Add(p);
+                                successful++;
+                            }
+                            catch (Exception ex)
+                            {
+                                Log_LB.Items.Add("Error: " + ex.Message);
+                            }
+                        }
+                        else
+                        {
+                            Log_LB.Items.Add("Patient missing identifier: " + p.Name.FirstOrDefault().Family);
+                        }
+                    }
+                    Log_LB.Items.Add("Number of Patients added: " + successful.ToString());
+                }
+
+            }
+
+            return result;
+        }
+
+        private int loadDeathTable(FhirClient iClient, List<Patient> iPatients)
+        {
+            int result = 0;
+
+            foreach(Patient p in iPatients)
+            {
+                // check to see if patient is deceased
+                if((FhirBoolean) p.Deceased == new FhirBoolean())
+                {
+                    ;
+                }
+            }
+
+            return result;
+        }
+
+        private void CHORD_VDWBuilder_Load(object sender, EventArgs e)
+        {
+            FHIR_URL.Text = "http://vonk.fire.ly";
+        }
+    }
+}
